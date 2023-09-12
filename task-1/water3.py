383198ba5998603a1fb3393d202621d28b5d3513
siwakorn-tin <siwakorntin@outlook.com>
2023-09-12 15:20:56 +0700

    submission Assignment1
    Submit all finished task

--- task-1/water3.py
+++ task-1/water3.py
@@ -1,9 +1,29 @@
 def initial_state():
-    return (0, 0, 0)
+    return (8, 0, 0)
 
 def is_goal(s):
-    return True
+    return s[0] == 4 and s[1] == 4
 
 def successors(s):
     x, y, z = s
-    return []
+    
+    def pour(source, target, max_amount):
+        if source > 0:
+            amount_to_pour = min(source, max_amount - target)
+            if amount_to_pour > 0:
+                new_source = source - amount_to_pour
+                new_target = target + amount_to_pour
+                yield ((new_source, new_target, z), amount_to_pour)
+
+    max_x, max_y, max_z = 8, 5, 3
+
+    yield from pour(x, y, max_y)
+    yield from pour(x, z, max_z)
+    yield from pour(y, x, max_x)
+    yield from pour(y, z, max_z)
+    yield from pour(z, x, max_x)
+    yield from pour(z, y, max_y)
+
+# Example usage:
+# for state, amount_poured in successors((8, 0, 0)):
+#     print(f"Pour {amount_poured} units: {state}")
--- task-2/eight_puzzle.py
+++ task-2/eight_puzzle.py
@@ -1,54 +1,41 @@
 import copy
 
-
 def initial_state():
     return ((7, 2, 4, 5, 0, 6, 8, 3, 1), 1, 1)
-
 
 def is_goal(s):
     return s[0] == (1, 2, 3, 4, 5, 6, 7, 8, 0)
 
-
 def successors(s):
     _, r, c = s
-    new_r, new_c = r-1, c
-    if is_valid(new_r, new_c):
-        yield move_blank(s, new_r, new_c), 1
-    new_r, new_c = r+1, c
-    if is_valid(new_r, new_c):
-        yield move_blank(s, new_r, new_c), 1
-    new_r, new_c = r, c-1
-    if is_valid(new_r, new_c):
-        yield move_blank(s, new_r, new_c), 1
-    new_r, new_c = r, c+1
-    if is_valid(new_r, new_c):
-        yield move_blank(s, new_r, new_c), 1
-
+    for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+        new_r, new_c = r + dr, c + dc
+        if is_valid(new_r, new_c):
+            yield move_blank(s, new_r, new_c), 1
 
 def is_valid(r, c):
-    return 0 <= r <= 2 and 0 <= c <= 2
-
+    return 0 <= r < 3 and 0 <= c < 3
 
 def move_blank(s, new_r, new_c):
     board, r, c = s
     new_board = list(board)
-    new_board[r*3 + c] = new_board[new_r*3 + new_c]
-    new_board[new_r*3 + new_c] = 0
+    index1, index2 = r * 3 + c, new_r * 3 + new_c
+    new_board[index1], new_board[index2] = new_board[index2], new_board[index1]
     return (tuple(new_board), new_r, new_c)
 
 def h1(s):
     goal = (1, 2, 3, 4, 5, 6, 7, 8, 0)
     board, _, _ = s
-    res = 0
-    # The for loop counts the number of elements that is different from
-    # the goal configuration.
-    # We start from index 1 to 8 because the blank is excluded.
-    for idx in range(1, 9):
-        if goal[idx] != board[idx]:
-            res += 1
-    return res
+    return sum(1 for i, tile in enumerate(board) if tile != goal[i])
 
 def h3(s):
-    # implement this function
     board, _, _ = s
-    return 0
+    res = 0
+    for i, tile in enumerate(board):
+        if tile == 0:
+            q2, r2 = 2, 2
+        else:
+            q2, r2 = (tile - 1) // 3, (tile - 1) % 3
+        q1, r1 = i // 3, i % 3
+        res += abs(q1 - q2) + abs(r1 - r2)
+    return res
